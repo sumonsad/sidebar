503699a9a773a67d18552ca7998a8b287fe53e40
sumonsad <sumonsad22518@gmail.com>
2022-10-20 10:50:50 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,18 @@
+// for(var i=1;i<=100;i=i+2){
+//     document.write(" "+i);
+// }
+// document.write('<br/>');
+
+// function add(){
+//     var a = 2;
+//     var b = 3;
+//     var c = a+b;
+//     document.write(c);
+// }
+// add();
+// document.write('<br/>');
+// function add1(x,y){
+//     var result = x+y;
+//     document.write(result);
+// }
+// add1(8,9);
